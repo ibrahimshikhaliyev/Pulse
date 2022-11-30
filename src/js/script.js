8b8700e1d0dbf7206f95dbc7bf0176158c00173e
shikhaliyev ibrahim <shikhaliyevibrahim1@gmail.com>
2022-11-30 19:10:27 +0400

    finished input mask

--- src/js/script.js
+++ src/js/script.js
@@ -39,19 +39,20 @@ const back_link= document.querySelectorAll('.catalog-item_back-link');
 const catalog_item_main= document.querySelectorAll('.catalog-item_main-content');
 const catalog_item_secondary= document.querySelectorAll('.catalog-item_secondary-content');
 
-for(let i=0; i<more_link.length;i++){
-    more_link[i].addEventListener('click',e=>{
+more_link.forEach((item,i)=>{
+    item.addEventListener('click',e=>{
         catalog_item_main[i].classList.remove('catalog-item_main-content_active');
         catalog_item_secondary[i].classList.add('catalog-item_secondary-content_active');
     })
-}
+})
 
-for(let i=0; i<back_link.length;i++){
-    back_link[i].addEventListener('click',e=>{
+back_link.forEach((item,i)=>{
+    item.addEventListener('click',e=>{
         catalog_item_main[i].classList.add('catalog-item_main-content_active');
         catalog_item_secondary[i].classList.remove('catalog-item_secondary-content_active')
     })
-}
+})
+
 
 
 // modal windows script
@@ -61,42 +62,81 @@ const consultation_btns=document.querySelectorAll('[data-modal="consultation"]')
 const overlay=document.querySelector('.overlay');
 const consultation_modal= document.querySelector('#consultation');
 const order_modal= document.querySelector('#order');
+const order_modal_subtitle=order_modal.querySelector('.modal_subtitle');
 const thanks_modal=document.querySelector('#thanks');
 const modal_close=document.querySelectorAll('.modal_close');
 const order_btns=document.querySelectorAll('[data-modal="order"]');
+const catalog_item_title=document.querySelectorAll('.catalog-item_title');
+
 
 window.addEventListener('click',e=>{
     if(e.target.classList.contains('overlay')){
-        overlay.style.display="none";
+        hide(overlay);
+        hide(order_modal);
+        hide(consultation_modal);
     }
 })
 
 
 consultation_btns.forEach(item=>{
     item.addEventListener('click' ,e=>{
-        overlay.style.display="block";
-        consultation_modal.style.display="block";
+        show(overlay);
+        show(consultation_modal);
+        // document.querySelector('#phone').focus()
     })
 })
 
 modal_close.forEach(item=>{
     item.addEventListener('click',e=>{
-        overlay.style.display="none";
+        hide(overlay);
+        hide(order_modal);
+        hide(consultation_modal)
     })
 })
 
 
 order_btns.forEach(item=>{
-    
     item.addEventListener('click' ,e=>{
-        overlay.style.display="block";
-        order_modal.style.display="block";
+        show(overlay);
+        show(order_modal);
+    })
+})
+
+order_btns.forEach((item,i)=>{
+    item.addEventListener('click' ,e=>{
+        order_modal_subtitle.innerHTML= catalog_item_title[i].innerHTML;
+        show(overlay);
+        show(order_modal);
     })
 })
 
 
+function show(item) {
+    item.style.display="block";
+}
+
+function hide(item) {
+    item.style.display="none";
+}
 
 
 
 
 
+
+
+
+
+var selector = document.querySelectorAll('input[name=phone]');
+
+var im = new Inputmask("+994 (88) 888-88-88");
+im.mask(selector);
+
+//or
+
+// Inputmask({"mask": "(999) 999-9999"}).mask(selector);
+// Inputmask("9-a{1,3}9{1,3}").mask(selector);
+// Inputmask("9", { repeat: 10 }).mask(selector);
+
+// Inputmask({ regex: "\\d*" }).mask(selector);
+// Inputmask({ regex: String.raw`\d*` }).mask(selector);
